59edfef02577f5ba32477587f28e2f4c1567938e
Samy <samy@gmail.com>
2022-03-31 08:31:10 +0200

    Add project files.

---
+++ ADemabre.IUT.Forum.Ui/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADemabre.IUT.Forum.Ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
