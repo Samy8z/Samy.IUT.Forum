899adbe390a660f8ae721ec4e07935b24d12c23e
Samy <samy@gmail.com>
2022-03-31 09:31:32 +0200

    - Edit Cat | 90%

--- ADemabre.IUT.Forum.Ui/Models/Categorie.cs
+++ ADemabre.IUT.Forum.Ui/Models/Categorie.cs
@@ -1,6 +1,33 @@
-﻿namespace ADemabre.IUT.Forum.Ui.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADemabre.IUT.Forum.Ui.Models
 {
     public class Categorie
     {
+        public int Id { get; set; }        
+        
+        [StringLength(255, MinimumLength = 3)]
+        [Required]
+        public string Nom { get; set; }
+
+        [StringLength(2000)]
+        public string Description { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-z][a-z\-]{3,}$", ErrorMessage = "La clé doit commencer par une lettre minuscule et non accentué et fait 3 lettres minimum")]
+        public string Cle { get; set; }
+
+        [Required]
+        public short Order { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime Creation { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime Modification { get; set; }
+        
+        public ICollection<Topic> Topics { get; set; }
     }
 }
